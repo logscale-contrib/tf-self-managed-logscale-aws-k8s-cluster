6b5e237107cfc0b15293d7cee583422fe62e622f
Ryan Faircloth <ryan@dss-i.com>
2022-12-09 19:35:32 -0500

    fix: missing files

--- outputs.tf
+++ outputs.tf
@@ -14,12 +14,15 @@ output "eks_cluster_oidc_issuer_url" {
   value = module.eks.cluster_oidc_issuer_url
 }
 output "karpenter_queue_name" {
-  value =module.karpenter.queue_name
+  value = module.karpenter.queue_name
 }
 output "karpenter_instance_profile_name" {
   value = module.karpenter.instance_profile_name
 }
 
+output "karpenter_irsa_arn" {
+  value = module.karpenter.irsa_arn
+}
 output "cluster_version" {
   value = module.eks.cluster_version
 }
